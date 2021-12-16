278b5b9dca0c564c5ab5f3c30a842abee56a76ab
Vinn09 <84385458+Vinn09@users.noreply.github.com>
2021-12-16 22:05:57 +0800

    Update Source Code.c

--- Source Code.c
+++ Source Code.c
@@ -72,9 +72,9 @@ void lihat_antrian()
     {
         printf("\nData antrian saat ini\n");    //menampilkan data yang ada pada antrian
         printf("\nNo Antrian\tNama Pelanggan\n");
-        for(i=head;i<=tail;i++)
+        for(int i=head;i<=tail;i++)
         {
-            printf("%d\t%s\n",data[i+1].nomor,data[i+1].nama);
+            printf("   %d\t\t  %s\n",data[i+1].nomor,data[i+1].nama);
         }
     }
 }
